fa9a59a008174917a1cb60674ae8021d36466fb9
Dwi <satya@codeid.onmicrosoft.com>
2023-03-21 16:58:32 +0700

    fix cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,7 +1,8 @@
 function middleware(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 }
 
