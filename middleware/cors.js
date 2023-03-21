f81a960d4768a542671af35f7abca8551a336641
Dwi <satya@codeid.onmicrosoft.com>
2023-03-21 17:01:05 +0700

    add cors custom field

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,7 +2,7 @@ function middleware(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization", "Pragma");
   next();
 }
 
