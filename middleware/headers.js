229be5ac3e44f6320efc5c9c0cd97a43746ee49c
Dwi <satya@codeid.onmicrosoft.com>
2023-03-21 16:54:44 +0700

    adapt activo header

--- middleware/headers.js
+++ middleware/headers.js
@@ -5,6 +5,9 @@ function middleware(req, res, next){
   res.header('Cache-Control','public');
   res.header('Server', 'Pelias/'+pkg.version);
   res.header('X-Powered-By', 'pelias');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 }
 
